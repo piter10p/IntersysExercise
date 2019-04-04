69a20264f18a1d34f7efbf23d88b35683648bb31
Piotr Paszko <piter9p@interia.pl>
2019-04-04 19:30:46 +0200

    Implemented point 3.

---
+++ IntersysExercise/IntersysExercise/Controllers/NumbersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace IntersysExercise.Controllers
+{
+    public class NumbersController : ApiController
+    {
+        [HttpGet]
+        public double GetData(double num)
+        {
+            return num * 2;
+        }
+    }
+}
